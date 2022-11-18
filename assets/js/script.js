2179489b2fac0a93e8ac8c7a23a69e225fac3414
cdennis27 <113468121+cdennis27@users.noreply.github.com>
2022-11-17 21:22:23 -0500

    Added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,43 @@
 // DAYJS
 var today = dayjs();
-  $('#current-day').text(today.format('[Date: ] MMMM D, YYYY'));
+$('#current-day').text(today.format('[Date: ] MMMM D, YYYY'));
 
 // ADVICESLIP API
-  // Variable
-  const adviceText = document.querySelector(".tip-day");
+// Variable
+const adviceText = document.querySelector(".tip-day");
 
-  // Run the showQuote function when the page is loaded
-  window.onload = showQuote;
+// Run the showQuote function when the page is loaded
+window.onload = showQuote()
 
-  // showQuote function to show random quote from API
-  function showQuote(){
-    fetch("https://api.adviceslip.com/advice")
+// showQuote function to show random quote from API
+function showQuote() {
+
+  fetch("https://api.adviceslip.com/advice")
     .then(response => response.json())
-    .then((data) => data.slip)
+    //.then((data) => data.slip)
     .then((data) => {
       console.log(data.slip);
-        adviceText.textContent = data.advice;
+      adviceText.textContent = data.slip.advice;
     })
     .catch((error) => {
       // Update this to MODAL!
-        alert(`Error ${error}`);
+      alert(`Error ${error}`);
     });
-  }
+}
 
 // NBP WEB API  
-  //Variable
-  const USD = document.querySelector("#currency-output");
+//Variable
+const USD = document.querySelector("#currency-output");
 
-  // Run the showUSD function when the page is loaded
-  window.onload = showUsd;
+// Run the showUSD function when the page is loaded
+window.onload = showUsd()
 
-  // showUSD function to show USD rate from API
-  function showUsd(){
+// showUSD function to show USD rate from API
+function showUsd() {
   fetch("https://api.nbp.pl/api/exchangerates/rates/c/usd/2016-04-04/?format=json")
     .then(response => response.json())
     .then((data) => {
+      console.log(data);
       USD.textContent = data.rates[0].ask;
-  })};
+    })
+};
